37b4bbf6d349e5a7f44418064def952eb69f764a
kylechui <kyle.chui@pm.me>
2021-06-05 21:31:23 -0700

    Recolour latex stuff

--- lua/pluginSettings/nvim-treesitter.lua
+++ lua/pluginSettings/nvim-treesitter.lua
@@ -1,5 +1,11 @@
 require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
+    custom_captures = {
+      ["punctuation.delimiter"] = "TSInclude",
+      ["punctuation.special"] = "TSInclude",
+      ["text.math"] = "TSFunction",
+      ["text.title"] = "TSParameter",
+    }
   },
 }
