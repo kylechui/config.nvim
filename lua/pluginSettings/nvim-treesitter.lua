4c896f1c196198f4c219444c1675967ad415de08
kylechui <kyle.chui@pm.me>
2021-07-06 21:20:54 -0700

    Fix annoying highlight bug with latex and treesitter

--- lua/pluginSettings/nvim-treesitter.lua
+++ lua/pluginSettings/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
+    additional_vim_regex_highlighting = true,
     custom_captures = {
       ["punctuation.delimiter"] = "TSInclude",
       ["punctuation.special"] = "TSInclude",
