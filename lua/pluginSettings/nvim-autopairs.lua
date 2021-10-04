a897aabf89aa97bd561a63e04e145333fcfa3d7e
kylechui <kyle.chui@pm.me>
2021-10-04 10:06:36 -0700

    Reconfig autopairs

--- lua/pluginSettings/nvim-autopairs.lua
+++ lua/pluginSettings/nvim-autopairs.lua
@@ -1,22 +1,19 @@
-local Rule = require('nvim-autopairs.rule')
-local npairs = require('nvim-autopairs')
-local cond = require('nvim-autopairs.conds')
--- Don't spawn a matching delimiter if cursor precedes alphanumeric character
-npairs.setup({
-  check_ts = true,
-  ignored_next_char = "[%w]",
-})
+require('nvim-autopairs').setup{}
+require('nvim-autopairs').remove_rule('"', {'tex', 'latex'})
 
--- I already have an UltiSnips macro for this
-npairs.remove_rule('"', {"tex", "latex"})
+local remap = vim.api.nvim_set_keymap
+local npairs = require('nvim-autopairs')
 
 -- skip it, if you use another global object
-_G.MUtils = {}
+_G.MUtils= {}
 
-MUtils.smart_cr=function()
+MUtils.completion_confirm=function()
   if vim.fn.pumvisible() ~= 0  then
-      return npairs.esc('<CR>')
+      return npairs.esc("<cr>")
   else
     return npairs.autopairs_cr()
   end
 end
+
+
+remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
