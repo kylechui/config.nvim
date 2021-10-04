1eab37c31a980df10be0cbd3b4a768e98f48f60a
kylechui <kyle.chui@pm.me>
2021-10-04 10:06:50 -0700

    Reconfig packer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,12 +1,9 @@
-local execute = vim.api.nvim_command
-local fn = vim.fn
-
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-    execute 'packadd packer.nvim'
-end
+vim.cmd([[
+augroup packerUserConfig
+  autocmd!
+  autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+augroup end
+]])
 
 return require('packer').startup(function()
     -- Packer can manage itself
