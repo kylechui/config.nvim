bf0fbadeeff21a6c1daecc5f75e0788bf1afb922
kylechui <kyle.chui@pm.me>
2022-02-04 21:57:53 -0800

    Setup Lua LSP and new rename var function.

--- lua/utils.lua
+++ lua/utils.lua
@@ -25,4 +25,20 @@ function M.get_help(file)
     vim.cmd(help_string)
 end
 
+-- Custom rename function to start with empty prompt
+function M.rename_var()
+    local position_params = vim.lsp.util.make_position_params()
+    local new_name = vim.fn.input("New name: ")
+
+    if (#new_name == 0) then
+        return
+    end
+
+    position_params.newName = new_name
+
+    vim.lsp.buf_request(0, 'textDocument/rename', position_params, function(err, method, result, ...)
+        vim.lsp.handlers['textDocument/rename'](err, method, result, ...)
+    end)
+end
+
 return M
