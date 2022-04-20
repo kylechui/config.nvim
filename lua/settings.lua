d158faeccd8daeffa4979e6ee7daec8e2052359e
kylechui <kyle.chui@pm.me>
2022-04-19 17:15:44 -0700

    docs(settings): Document what settings actually do.

--- lua/settings.lua
+++ lua/settings.lua
@@ -4,35 +4,48 @@ local opt = vim.opt
 -- Define workspace path
 vim.env.WORKSPACE = "~/Documents/github/"
 
+-- Set Neovim to use the system clipboard
 opt.clipboard = "unnamedplus"
+-- Some execution-related settings
 opt.modelines = 0
 opt.maxmempattern = 2500
--- GUI stuff
-vim.cmd("colorscheme kanagawa")
-
-opt.guifont = "JetBrains Mono:h12"
+-- Setup colorscheme
+vim.g.colors_name = "kanagawa"
+opt.background = "dark"
 opt.termguicolors = true
+-- Set font for GUIs (I use Neovide)
+opt.guifont = "JetBrains Mono:h12"
+-- Allow mouse use in all modes
 opt.mouse = "a"
+-- Line number settings
 opt.number = true
 opt.relativenumber = true
+-- Set cursor line settings
 opt.wrap = true
 opt.cursorline = true
 opt.breakindent = true
 opt.linebreak = true
+-- Add a color column at 80 characters
+opt.colorcolumn = "80"
+-- Always show 8 lines of "context" above and below the cursor
 opt.scrolloff = 8
+-- Configure tab settings
 opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 opt.shiftround = true
-opt.viewoptions = "folds,cursor"
+-- Only save information about the cursor and folds on exit
+opt.viewoptions = "cursor,folds"
+-- Setup smarter search settings
 opt.ignorecase = true
 opt.smartcase = true
-opt.colorcolumn = "80"
-opt.laststatus = 3
-
-opt.lazyredraw = true
 opt.hlsearch = false
+-- Use global statusline
+opt.laststatus = 3
+-- Redraw the screen less during computationally intensive tasks
+opt.lazyredraw = true
+-- Force vertical splits to always open on the right
 opt.splitright = true
 opt.completeopt = "menu,menuone,noselect"
 -- Neovide-specific cursor settings
