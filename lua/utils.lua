6a751e545c4781152531dc9198138c5d53042528
kylechui <kyle.chui@pm.me>
2022-04-19 19:44:28 -0700

    chore(utils): Remove unused code.

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,23 +1,11 @@
 local M = {}
 
-function M.is_buffer_empty()
-    -- Check whether the current buffer is empty
-    return vim.fn.empty(vim.fn.expand("%:t")) == 1
-end
-
+-- Change the font size by some amount delta
 function M.adjust_font_size(delta)
     local old_font = vim.opt.guifont["_value"]
     local font_size = string.match(old_font, ":h(%d+)$")
     local new_font = string.gsub(old_font, ":h(%d+)$", ":h" .. (font_size + delta))
     vim.opt.guifont = new_font
-end
-
-function M.get_help(file)
-    local help_string = "h " .. file
-    if M.is_buffer_empty() then
-        help_string = help_string .. " | only"
-    end
-    vim.cmd(help_string)
 end
 
 -- Custom rename function to start with empty prompt
