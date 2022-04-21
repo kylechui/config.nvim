281e86197c31219ec8e36f8cfed41dd2b74f8acc
kylechui <kyle.chui@pm.me>
2022-04-21 12:21:56 -0700

    style(LSP): Extract keybinds into new function.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,15 +1,21 @@
 local map = vim.keymap.set
 
+local setup_lsp_keybinds = function()
+    map("n", "<Leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
+    map("n", "<Leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
+    map("n", "<Leader>dl", require("telescope.builtin").diagnostics, { buffer = 0 })
+    map("n", "<Leader>r", require("utils").rename_var, { buffer = 0 })
+    map("n", "<Leader>c", vim.lsp.buf.code_action, { buffer = 0 })
+end
+
 require("lspconfig").clangd.setup({
     on_attach = function(client)
-        map("n", "<Leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
-        map("n", "<Leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
-        map("n", "<Leader>dl", require("telescope.builtin").diagnostics, { buffer = 0 })
-        map("n", "<Leader>r", require("utils").rename_var, { buffer = 0 })
-        map("n", "<Leader>c", vim.lsp.buf.code_action, { buffer = 0 })
-
+        setup_lsp_keybinds()
         client.resolved_capabilities.document_formatting = false
         client.resolved_capabilities.document_range_formatting = false
+        --[[ -- Preparing for when nvim-lspconfig updates to 0.7 APIs
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false ]]
     end,
 })
 
@@ -17,11 +23,7 @@ local sumneko_root = os.getenv("HOME") .. "/lua-language-server"
 
 require("lspconfig").sumneko_lua.setup({
     on_attach = function()
-        map("n", "<Leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
-        map("n", "<Leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
-        map("n", "<Leader>dl", require("telescope.builtin").diagnostics, { buffer = 0 })
-        map("n", "<Leader>r", require("utils").rename_var, { buffer = 0 })
-        map("n", "<Leader>c", vim.lsp.buf.code_action, { buffer = 0 })
+        setup_lsp_keybinds()
     end,
     cmd = {
         sumneko_root .. "/bin/lua-language-server",
