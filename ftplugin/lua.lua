f7cbde5dcdd8ad4eabdc171db1606926ecd113c7
kylechui <kyle.chui+github@pm.me>
2022-07-12 00:20:56 -0700

    feat: Test a project with plenary from any file.

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,6 +1,20 @@
 local map = vim.keymap.set
 
-map("n", "<Leader>t", "<Plug>PlenaryTestFile", {
+map("n", "<Leader>t", function()
+    local cur_path = vim.fn.expand("%:p:h")
+    while cur_path and vim.fn.finddir("tests", cur_path) == "" do
+        cur_path = cur_path:match("(.*)/.*")
+    end
+
+    if cur_path then
+        cur_path = cur_path .. "/tests"
+        local file_name = vim.fn.systemlist("ls " .. cur_path)[1]
+        require("plenary.test_harness").test_directory(vim.fn.findfile(
+            file_name,
+            cur_path)
+        )
+    end
+end, {
     silent = true,
     buffer = true,
 })
