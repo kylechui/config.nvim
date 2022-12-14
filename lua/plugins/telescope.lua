2926d0e80eac8c511eca425aec9463365ec21a87
kylechui <kyle.chui+github@pm.me>
2022-12-14 00:09:31 -0800

    feat(plugin): Additional Telescope niceties.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,7 +14,27 @@ telescope.setup({
                 ["<C-j>"] = actions.move_selection_next,
                 ["<C-k>"] = actions.move_selection_previous,
             },
+            n = {
+                ["q"] = require("telescope.actions").close,
+            },
         },
+        prompt_prefix = "   ",
+        selection_caret = "  ",
+        sorting_strategy = "ascending",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.6,
+                results_width = 0.25,
+            },
+            vertical = {
+                mirror = false,
+            },
+            width = 0.85,
+            height = 0.85,
+            preview_cutoff = 60,
+        },
+        border = {},
     },
 })
 
