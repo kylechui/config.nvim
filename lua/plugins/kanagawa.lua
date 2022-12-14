a8354e63597739233a03f6da1f0448987a1577ab
kylechui <kyle.chui+github@pm.me>
2022-12-13 23:56:28 -0800

    feat(plugin): Telescope theming.

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -7,5 +7,16 @@ end
 kanagawa.setup({
     globalStatus = true,
 })
+local hl = vim.api.nvim_set_hl
 -- Emphasize window separators
-vim.api.nvim_set_hl(0, "WinSeparator", { fg = "NONE" })
+hl(0, "WinSeparator", { fg = "NONE" })
+
+-- Telescope theming
+local colors = require("kanagawa.colors").setup()
+hl(0, "TelescopeBorder", { fg = colors.sumiInk0, bg = colors.sumiInk0 })
+hl(0, "TelescopeNormal", { fg = colors.fujiWhite, bg = colors.sumiInk0 })
+hl(0, "TelescopePromptBorder", { fg = colors.sumiInk2, bg = colors.sumiInk2 })
+hl(0, "TelescopePromptNormal", { fg = colors.fujiWhite, bg = colors.sumiInk2 })
+hl(0, "TelescopeSelection", { fg = colors.fujiWhite, bg = colors.sumiInk2 })
+hl(0, "TelescopePreviewTitle", { fg = colors.sumiInk2, bg = colors.oniViolet })
+hl(0, "TelescopePromptTitle", { fg = colors.sumiInk2, bg = colors.sakuraPink })
