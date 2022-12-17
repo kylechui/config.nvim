d21e1a0637d38b44822c4d93d3712f2cdf9c3960
kylechui <kyle.chui+github@pm.me>
2022-12-16 18:46:16 -0800

    feat(plugin): Configure surround in separate file.

--- after/plugin/nvim-surround.lua
+++ after/plugin/nvim-surround.lua
@@ -1,8 +1,27 @@
 require("nvim-surround").setup({
-    keymaps = {
-        insert = "ys",
-        visual = "S",
-        delete = "ds",
-        change = "cs",
-    }
+    surrounds = {
+        ["f"] = {
+            change = {
+                target = function()
+                    local selection = require("nvim-surround.config").get_selection({
+                        query = {
+                            capture = "@function_name",
+                            type = "nvim-surround",
+                        },
+                    })
+                    if not selection then
+                        return nil
+                    end
+                    local func_name = require("nvim-surround.config").get_input("Enter a function name: ")
+                    return {
+                        left = func_name,
+                        right = {
+                            first_pos = { selection.last_pos[1], selection.last_pos[2] + 1 },
+                            last_pos = selection.last_pos,
+                        },
+                    }
+                end,
+            },
+        },
+    },
 })
