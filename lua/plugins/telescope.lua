492358a1267e0787aaa76e8b9d1de0575c0b0e6a
kylechui <kyle.chui+github@pm.me>
2023-03-19 09:01:04 -0700

    fix: Move colorscheme details back to `kanagawa`.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,17 +5,6 @@ return {
         keys = { "<Leader>f", "<Leader>s" },
         dependencies = "nvim-lua/plenary.nvim",
         config = function()
-            local hl = vim.api.nvim_set_hl
-            local palette = require("kanagawa.colors").setup().palette
-            hl(0, "WinSeparator", { fg = "NONE" })
-            hl(0, "TelescopeBorder", { fg = palette.sumiInk0, bg = palette.sumiInk0 })
-            hl(0, "TelescopeNormal", { fg = palette.fujiWhite, bg = palette.sumiInk0 })
-            hl(0, "TelescopePromptBorder", { fg = palette.sumiInk4, bg = palette.sumiInk4 })
-            hl(0, "TelescopePromptNormal", { fg = palette.fujiWhite, bg = palette.sumiInk4 })
-            hl(0, "TelescopeSelection", { fg = palette.fujiWhite, bg = palette.sumiInk4 })
-            hl(0, "TelescopePreviewTitle", { fg = palette.sumiInk4, bg = palette.oniViolet })
-            hl(0, "TelescopePromptTitle", { fg = palette.sumiInk4, bg = palette.sakuraPink })
-
             local actions = require("telescope.actions")
             require("telescope").setup({
                 defaults = {
