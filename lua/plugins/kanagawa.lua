6922ac08419fdaf48156b5557f328ae56d412f5a
kylechui <kyle.chui+github@pm.me>
2023-03-20 16:17:26 -0700

    refactor: kanagawa setup.

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -2,19 +2,22 @@ return {
     "rebelot/kanagawa.nvim",
     priority = 1000,
     opts = {
-        globalStatus = true,
+        overrides = function()
+            local palette = require("kanagawa.colors").setup().palette
+            return {
+                WinSeparator = { fg = "NONE" },
+                TelescopeBorder = { fg = palette.sumiInk0, bg = palette.sumiInk0 },
+                TelescopeNormal = { fg = palette.fujiWhite, bg = palette.sumiInk0 },
+                TelescopePromptBorder = { fg = palette.sumiInk4, bg = palette.sumiInk4 },
+                TelescopePromptNormal = { fg = palette.fujiWhite, bg = palette.sumiInk4 },
+                TelescopeSelection = { fg = palette.fujiWhite, bg = palette.sumiInk4 },
+                TelescopePreviewTitle = { fg = palette.sumiInk4, bg = palette.oniViolet },
+                TelescopePromptTitle = { fg = palette.sumiInk4, bg = palette.sakuraPink },
+            }
+        end,
     },
-    config = function()
+    config = function(_, opts)
+        require("kanagawa").setup(opts)
         vim.cmd.colorscheme("kanagawa")
-        local hl = vim.api.nvim_set_hl
-        local palette = require("kanagawa.colors").setup().palette
-        hl(0, "WinSeparator", { fg = "NONE" })
-        hl(0, "TelescopeBorder", { fg = palette.sumiInk0, bg = palette.sumiInk0 })
-        hl(0, "TelescopeNormal", { fg = palette.fujiWhite, bg = palette.sumiInk0 })
-        hl(0, "TelescopePromptBorder", { fg = palette.sumiInk4, bg = palette.sumiInk4 })
-        hl(0, "TelescopePromptNormal", { fg = palette.fujiWhite, bg = palette.sumiInk4 })
-        hl(0, "TelescopeSelection", { fg = palette.fujiWhite, bg = palette.sumiInk4 })
-        hl(0, "TelescopePreviewTitle", { fg = palette.sumiInk4, bg = palette.oniViolet })
-        hl(0, "TelescopePromptTitle", { fg = palette.sumiInk4, bg = palette.sakuraPink })
     end,
 }
