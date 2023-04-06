74c5afd2b39d8d7863a4b8d582043acabd3aae50
kylechui <kyle.chui+github@pm.me>
2023-04-05 20:46:01 -0700

    fix: Load Comment.nvim on `gc` and `gb`.

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -3,6 +3,8 @@ return {
     keys = {
         { mode = "n", "<C-/>", "<Plug>(comment_toggle_linewise_current)" },
         { mode = "x", "<C-/>", "<Cmd>norm gbgv<CR>" },
+        "gc",
+        "gb",
     },
     config = true,
 }
