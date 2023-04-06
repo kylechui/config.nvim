86c31e5dbf7aaf4ca4fd28afaec1ee35dd7542a5
kylechui <kyle.chui+github@pm.me>
2023-04-05 20:45:48 -0700

    feat: Use Haskell plugin.

---
+++ lua/plugins/haskell-tools.lua
@@ -0,0 +1,9 @@
+return {
+    "mrcjkb/haskell-tools.nvim",
+    ft = "haskell",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    branch = "1.x.x",
+}
