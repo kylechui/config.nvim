087f34165fd51815ac8807bca211eddcbdd69613
Kyle Chui <kyle.chui+github@pm.me>
2023-05-29 00:11:54 -0700

    chore: Update snippets.

--- luasnippets/tex/in_text.lua
+++ luasnippets/tex/in_text.lua
@@ -40,44 +40,43 @@ return {
         i(0),
         t("}"),
     }, { condition = in_text }),
-},
-    {
-        -- LaTeX: Inline math mode
-        s("mm", fmt("${}$", i(1)), { condition = in_text }),
-        -- LaTeX: Display math mode
-        s("dm", {
-            t({ "\\[", "\t" }),
-            i(0),
-            t({ "", "\\]" }),
-        }, { condition = in_text }),
-        -- LaTeX: Single-letter variables
-        s(
-            { trig = " ([b-zB-Z])([%p%s])", regTrig = true, wordTrig = false },
-            f(function(_, snip)
-                return " $" .. snip.captures[1] .. "$" .. snip.captures[2]
-            end),
-            { condition = in_text }
-        ),
-        -- LaTeX: Quotations
-        s('"', fmt([[``{}'']], i(1)), { condition = in_text }),
-        -- LaTeX: Emphasis
-        s("emph", fmt([[\emph{{{}}}]], i(1)), { condition = in_text }),
-        -- LaTeX: Boldface
-        s("bf", fmt([[\textbf{{{}}}]], i(1)), { condition = in_text }),
-        -- LaTeX: Teletype
-        s("tt", fmt([[\texttt{{{}}}]], i(1)), { condition = in_text }),
-        -- LaTeX: Ordinal nth
-        s({ trig = "([%d$])th", regTrig = true, wordTrig = false }, {
-            f(function(_, snip)
-                return snip.captures[1] .. "\\tsup{th}"
-            end),
-        }, { condition = in_text }),
-        -- LaTeX: Image
-        s("img", {
-            t({ "\\begin{center}", "\t\\resizebox{" }),
-            i(1, "5"),
-            t("in}{!}{\\includegraphics{./"),
-            i(2),
-            t({ "}}", "\\end{center}" }),
-        }, { condition = in_text }),
-    }
+}, {
+    -- LaTeX: Inline math mode
+    s("mm", fmt("${}$", i(1)), { condition = in_text }),
+    -- LaTeX: Display math mode
+    s("dm", {
+        t({ "\\[", "\t" }),
+        i(0),
+        t({ "", "\\]" }),
+    }, { condition = in_text }),
+    -- LaTeX: Single-letter variables
+    s(
+        { trig = " ([b-hj-zB-HJ-Z])([%p%s])", regTrig = true, wordTrig = false },
+        f(function(_, snip)
+            return " $" .. snip.captures[1] .. "$" .. snip.captures[2]
+        end),
+        { condition = in_text }
+    ),
+    -- LaTeX: Quotations
+    s('"', fmt([[``{}'']], i(1)), { condition = in_text }),
+    -- LaTeX: Emphasis
+    s("emph", fmt([[\emph{{{}}}]], i(1)), { condition = in_text }),
+    -- LaTeX: Boldface
+    s("bf", fmt([[\textbf{{{}}}]], i(1)), { condition = in_text }),
+    -- LaTeX: Teletype
+    s("tt", fmt([[\texttt{{{}}}]], i(1)), { condition = in_text }),
+    -- LaTeX: Ordinal nth
+    s({ trig = "([%d$])th", regTrig = true, wordTrig = false }, {
+        f(function(_, snip)
+            return snip.captures[1] .. "\\tsup{th}"
+        end),
+    }, { condition = in_text }),
+    -- LaTeX: Image
+    s("img", {
+        t({ "\\begin{center}", "\t\\resizebox{" }),
+        i(1, "5"),
+        t("in}{!}{\\includegraphics{./"),
+        i(2),
+        t({ "}}", "\\end{center}" }),
+    }, { condition = in_text }),
+}
