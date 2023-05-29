214fb4955020fc752671198d1725dc84e325f8a3
Kyle Chui <kyle.chui+github@pm.me>
2023-05-29 00:11:18 -0700

    refactor: Migrate Obsidian keymaps into configuration.

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -9,33 +9,6 @@ vim.keymap.set("n", "<Leader>u", function()
     require("cmp_dictionary").update()
 end, { buffer = true })
 
-vim.keymap.set("n", "gf", function()
-    if require("obsidian").util.cursor_on_markdown_link() then
-        return "<Cmd>ObsidianFollowLink<CR>"
-    else
-        return "gf"
-    end
-end, { buffer = true, silent = true, noremap = false, expr = true })
-vim.keymap.set("n", "<Localleader>n", function()
-    local title = vim.fn.input("Note title: ")
-    if title then
-        return "<Cmd>ObsidianNew " .. title .. "<CR>"
-    end
-end, { buffer = true, silent = true, expr = true })
-vim.keymap.set("n", "<Localleader>o", function()
-    return "<Cmd>ObsidianOpen<CR>"
-end, { buffer = true, silent = true, expr = true })
-vim.keymap.set("x", "<Localleader>ll", function()
-    local title = vim.fn.input("Note title: ")
-    if title then
-        return ":ObsidianLink " .. title .. "<CR>"
-    end
-end, { buffer = true, silent = true, expr = true })
-vim.keymap.set("x", "<Localleader>ln", function()
-    local title = vim.fn.input("Note title: ")
-    return ":ObsidianLinkNew " .. title .. "<CR>"
-end, { buffer = true, silent = true, expr = true })
-
 require("nvim-surround").buffer_setup({
     surrounds = {
         ["l"] = {
--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,6 +1,41 @@
 return {
     "epwalsh/obsidian.nvim",
-    ft = { "markdown", "NvimTree" },
+    ft = { "markdown" },
+    keys = {
+        {
+            "gf",
+            function()
+                if require("obsidian").util.cursor_on_markdown_link() then
+                    return "<Cmd>ObsidianFollowLink<CR>"
+                else
+                    return "gf"
+                end
+            end,
+        },
+        {
+            "<Localleader>n",
+            function()
+                local title = vim.fn.input("Note title: ")
+                if title ~= "" then
+                    return "<Cmd>ObsidianNew " .. title .. "<CR>"
+                end
+            end,
+        },
+        {
+            "<Localleader>o",
+            "<Cmd>ObsidianOpen<CR>",
+        },
+        -- vim.keymap.set("x", "<Bslash>ll", function()
+        --     local title = vim.fn.input("Note title: ")
+        --     if title then
+        --         return ":ObsidianLink " .. title .. "<CR>"
+        --     end
+        -- end, { buffer = true, silent = true, expr = true })
+        -- vim.keymap.set("x", "<Bslash>ln", function()
+        --     local title = vim.fn.input("Note title: ")
+        --     return ":ObsidianLinkNew " .. title .. "<CR>"
+        -- end, { buffer = true, silent = true, expr = true })
+    },
     version = "v1.*",
     config = function()
         require("obsidian").setup({
