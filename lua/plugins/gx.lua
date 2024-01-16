95b1ad9e87a699ceb447c44748e77fede3b847b3
Kyle Chui <kyle.chui+github@pm.me>
2024-01-16 13:33:28 -0800

    chore: Update `gx`.

--- lua/plugins/gx.lua
+++ lua/plugins/gx.lua
@@ -1,6 +1,14 @@
 return {
     [1] = "chrishrb/gx.nvim",
-    version = "^0.4.0",
-    keys = { "gx" },
+    version = "^0.5.0",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+        {
+            "gx",
+            function()
+                vim.cmd.Browse()
+            end,
+        },
+    },
     config = true,
 }
