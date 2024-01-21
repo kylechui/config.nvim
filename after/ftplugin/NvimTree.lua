d9a8e0526bb129fdc03346788ed81ea97e6671c9
Kyle Chui <kyle.chui+github@pm.me>
2024-01-21 12:44:42 -0800

    refactor: Remove deprecated file.

--- after/ftplugin/NvimTree.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set("n", "<Localleader>n", function()
-    local title = vim.fn.input("Note title: ")
-    return "<Cmd>ObsidianNew " .. title .. "<CR>"
-end, { buffer = true, silent = true, expr = true })
