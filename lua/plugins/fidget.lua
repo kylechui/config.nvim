9a29b2ef0b4536624464d6d3a66fe39cbc58cd25
Kyle Chui <kyle.chui+github@pm.me>
2024-01-22 12:15:28 -0800

    feat(plugin): Add `fidget.nvim`.

---
+++ lua/plugins/fidget.lua
@@ -0,0 +1,11 @@
+return {
+    [1] = "j-hui/fidget.nvim",
+    version = "^1.2.0",
+    opts = {
+        progress = {
+            ignore = {
+                "null-ls",
+            },
+        },
+    },
+}
