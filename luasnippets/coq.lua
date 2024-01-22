7ba2ba979a6405b11930e2cba96ae4817e93c056
Kyle Chui <kyle.chui+github@pm.me>
2024-01-22 12:15:38 -0800

    feat(snippets): Add `coq` quantum snippets.

--- luasnippets/coq.lua
+++ luasnippets/coq.lua
@@ -1,5 +1,21 @@
 return nil,
     {
+        s({ trig = "ket", wordTrig = true }, {
+            t("∣"),
+            i(1),
+            t("⟩"),
+        }),
+        s({ trig = "bra", wordTrig = true }, {
+            t("⟨"),
+            i(1),
+            t("∣"),
+        }),
+        s({ trig = "\\psi", wordTrig = true }, {
+            t("ψ"),
+        }),
+        s({ trig = "\\phi", wordTrig = true }, {
+            t("φ"),
+        }),
         s({ trig = "xx", wordTrig = true }, {
             t("×"),
         }),
