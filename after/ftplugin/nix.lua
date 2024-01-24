a9c0c235d3df1b4cca794894dacec4b2d3642877
Kyle Chui <kyle.chui+github@pm.me>
2024-01-23 16:00:32 -0800

    feat: Add interpolation surround.

--- after/ftplugin/nix.lua
+++ after/ftplugin/nix.lua
@@ -1,3 +1,13 @@
 vim.bo.tabstop = 2
 vim.bo.softtabstop = 2
 vim.bo.shiftwidth = 2
+
+require("nvim-surround").buffer_setup({
+    surrounds = {
+        ["$"] = {
+            add = { "${", "}" },
+            find = "$%b{}",
+            delete = "^(%${)().-(})()$",
+        },
+    },
+})
