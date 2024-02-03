409539e18cc8fe1c3135f4209ac26e316d150774
Kyle Chui <kyle.chui+github@pm.me>
2024-02-03 10:45:01 -0800

    feat(coq): Smart LSP keymap.

--- after/ftplugin/coq.lua
+++ after/ftplugin/coq.lua
@@ -18,6 +18,11 @@ vim.keymap.set("n", "<M-r>", function()
     local current_buf = vim.api.nvim_get_current_buf()
     -- Close all buffers except the current one
     vim.cmd("bufdo if bufnr() != " .. current_buf .. " | bd | endif")
-    -- Restart LSP (assuming you are using 'nvim-lspconfig')
-    vim.cmd.LspRestart()
+    -- Start/restart LSP (assuming you are using 'nvim-lspconfig')
+    local coq_lsp_running = #vim.lsp.get_clients({ bufnr = current_buf, name = "coq_lsp" }) == 1
+    if coq_lsp_running then
+        vim.cmd.LspRestart()
+    else
+        vim.cmd.LspStart()
+    end
 end)
