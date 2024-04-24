437db13db2c7b706994e79f39cc5ba4663fd159c
Kyle Chui <kyle.chui+github@pm.me>
2024-04-23 17:11:20 -0700

    refactor: Remove unnecessary curly braces.

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,10 +1,8 @@
 return {
-    {
-        dir = "/home/kylec/Documents/github/nvim-surround/main",
-        event = "VeryLazy",
-        config = function()
-            vim.cmd.highlight("default link NvimSurroundHighlight Todo")
-            require("nvim-surround").setup()
-        end,
-    },
+    dir = "/home/kylec/Documents/github/nvim-surround/main",
+    event = "VeryLazy",
+    config = function()
+        vim.cmd.highlight("default link NvimSurroundHighlight Todo")
+        require("nvim-surround").setup()
+    end,
 }
