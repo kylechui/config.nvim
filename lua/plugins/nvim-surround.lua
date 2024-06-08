2a6fef9c75b31e330a4c13ac2d49f79661fa96bd
Kyle Chui <kyle.chui+github@pm.me>
2024-06-08 14:30:08 -0700

    feat(surround): Use `sticky` cursor.

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -2,6 +2,8 @@ return {
     dir = "/home/kylec/Documents/github/nvim-surround/main",
     event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup()
+        require("nvim-surround").setup({
+            move_cursor = "sticky",
+        })
     end,
 }
